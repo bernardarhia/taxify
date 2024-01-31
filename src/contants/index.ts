ccbaa66cc2ea94e9849003dba486fabbe0f30a7d
Bernard Arhia <megamindcooperations@gmail.com>
2024-01-31 10:44:11 +0000

    updated tax rates per month

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -2,12 +2,13 @@
 const SSNIT_RATE = 5.5;
 const NUM_OF_MONTHS = 12;
 const MONTHLY_TAX_RATES = [
-  [0, 319],
-  [5, 100],
-  [10, 120],
-  [17.5, 3000],
-  [25, 16461],
-  [30, Number.POSITIVE_INFINITY], // anything above GHC 20,000
+  [0, 490],
+  [5, 110],
+  [10, 130],
+  [17.5, 3166.67],
+  [25, 16000],
+  [30, 30520],
+  [35, Number.POSITIVE_INFINITY], // anything above GHC 50,000
 ];
 
-  export {SSNIT_RATE, MONTHLY_TAX_RATES, NUM_OF_MONTHS}
+  export { SSNIT_RATE, MONTHLY_TAX_RATES, NUM_OF_MONTHS }
